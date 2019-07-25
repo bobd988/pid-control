30a4d1df4c2f116f5b00d3cfc7f4dd9211e7938f
bobd988 <4655609+bobd988@users.noreply.github.com>
2019-07-24 21:16:42 -0700

    v2

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,30 +1,53 @@
 #include "PID.h"
 
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
+#include <algorithm>
+// using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
 
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+void PID::Init(double Kp, double Ki, double Kd) {
+  _Kp = Kp;
+  _Ki = Ki;
+  _Kd = Kd;
 
+  _p_error = 0.0;
+  _i_error = 0.0;
+  _d_error = 0.0;
+
+  // Previous cte.
+  _prev_cte = 0.0;
+
+  // Counters.
+  _count = 0;
+  _error_sum = 0.0;
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
+  // Proportional error.
+  _p_error = cte;
+
+  // Integral error.
+  _i_error += cte;
+
+  // Diferential error.
+  _d_error = cte - _prev_cte;
+  _prev_cte = cte;
+
+  _error_sum += cte;
+  _count++;
 
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+  return _p_error * _Kp + _i_error * _Ki + _d_error * _Kd;
+}
+
+double PID::AverageError() {
+  return _error_sum/_count;
 }
--- src/PID.h
+++ src/PID.h
@@ -2,49 +2,52 @@
 #define PID_H
 
 class PID {
- public:
-  /**
-   * Constructor
-   */
+private:
+  // Error
+  double _p_error;
+  double _i_error;
+  double _d_error;
+  double _prev_cte;
+
+  long _count;
+  double _error_sum;
+
+  double _Kp;
+  double _Ki;
+  double _Kd;
+
+public:
+
+  /*
+  * Constructor
+  */
   PID();
 
-  /**
-   * Destructor.
-   */
+  /*
+  * Destructor.
+  */
   virtual ~PID();
 
-  /**
-   * Initialize PID.
-   * @param (Kp_, Ki_, Kd_) The initial PID coefficients
-   */
-  void Init(double Kp_, double Ki_, double Kd_);
+  /*
+  * Initialize PID.
+  */
+  void Init(double Kp, double Ki, double Kd);
 
-  /**
-   * Update the PID error variables given cross track error.
-   * @param cte The current cross track error
-   */
+  /*
+  * Update the PID error variables given cross track error.
+  */
   void UpdateError(double cte);
 
-  /**
-   * Calculate the total PID error.
-   * @output The total PID error
-   */
+  /*
+  * Calculate the total PID error.
+  */
   double TotalError();
 
- private:
-  /**
-   * PID Errors
-   */
-  double p_error;
-  double i_error;
-  double d_error;
+  /*
+  *  Returns the average error.
+  */
+  double AverageError();
 
-  /**
-   * PID Coefficients
-   */ 
-  double Kp;
-  double Ki;
-  double Kd;
 };
 
-#endif  // PID_H
+#endif /* PID_H */
